e098470ac9bae2f55826509dc2797ae78b340a0d
Yukari Chess <yukarichess@gmail.com>
2026-04-05 18:30:15 +0100

    Increase negative extension
    bench: 915246

--- yukari/src/search.rs
+++ yukari/src/search.rs
@@ -499,7 +499,11 @@ impl Thread {
                     if score < singular_beta {
                         extension += 1;
                     } else if tt_entry.score as i32 >= beta {
-                        extension -= 1;
+                        if expected_pvnode {
+                            extension -= 1;   
+                        } else {
+                            extension -= 2;
+                        }
                     }
                 // Low depth singular extension: Determine singularity by static eval vs alpha.
                 } else if !self.board[ply].in_check() && depth <= 7 && eval <= alpha - 25 && tt_entry.flags == TtFlags::Lower {
